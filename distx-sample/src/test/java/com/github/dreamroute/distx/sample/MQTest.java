a4bb9e29165716febdd27194a28815dfcec9ac35
w.dehai <342252328@qq.com>
2021-02-26 12:42:08 +0800

    fix: 移除无用代码

--- distx-sample/src/test/java/com/github/dreamroute/distx/sample/MQTest.java
+++ distx-sample/src/test/java/com/github/dreamroute/distx/sample/MQTest.java
@@ -1,9 +1,7 @@
 package com.github.dreamroute.distx.sample;
 
-import com.github.dreamroute.distx.sample.consumer.Abc;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.support.MessageBuilder;
 
@@ -14,12 +12,9 @@ class MQTest {
 
     @Resource
     private RocketMQTemplate rocketMQTemplate;
-    @Autowired
-    private Abc abc;
 
     @Test
     void sendTest() {
-        System.err.println(abc);
         rocketMQTemplate.send("TopicTest", MessageBuilder.withPayload("fuyun").build());
     }
 
